9367d0ffa28187b9f594204cd9b70022a647c718
Timothy Ng <timng>
2024-07-01 20:34:02 +0000

    forest: edit server.py

--- questions/graph/forest/server.py
+++ questions/graph/forest/server.py
@@ -1,10 +1,6 @@
-import random, copy, math
+import random
 import prairielearn as pl
-import numpy as np
-import networkx as nx
-from networkx.generators.random_graphs import fast_gnp_random_graph, connected_watts_strogatz_graph
-from networkx.drawing.nx_agraph import write_dot
-from networkx.algorithms.connectivity import minimum_edge_cut
+from sympy import sympify
 
 def generate(data):
     n = random.randint(20,200)
@@ -15,5 +11,5 @@ def generate(data):
     
     ans = n-k
     
-    data['correct_answers']['n'] = ans
+    data['correct_answers']['n'] = pl.to_json(sympify(ans))
 
